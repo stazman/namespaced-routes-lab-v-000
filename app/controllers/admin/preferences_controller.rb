18b32993341ac672238861217b2f68f16695cb53
github username <github email address>
2018-06-13 13:52:54 -0500

    Done.

---
+++ app/controllers/admin/preferences_controller.rb
@@ -0,0 +1,7 @@
+class Admin::PreferencesController < ApplicationController
+
+    def index
+    end
+
+    #strong params
+end
---
+++ app/models/preference.rb
@@ -0,0 +1,6 @@
+class Preference < ActiveRecord::Base
+#Song sort order (e.g. "ASC" or "DESC"). This will be used to control the default sort order on the /songs page.
+# Artist sort order. This will be used to control the default sort order on the /artists page.
+# Allowing creation of new songs. Used to control the ability to add new songs to the system.
+# Allowing creation of new artists. Used to control the ability to add new artists to the system.
+end
